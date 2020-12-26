11e47c5095dd802114895e1d66810ab11d1859a4
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-12-26 16:28:05 +0100

    Add git config

--- users/pn.nix
+++ users/pn.nix
@@ -1,4 +1,5 @@
 ## My default user
+{ pkgs, ... }:
 
 let
   home-manager = builtins.fetchTarball {
@@ -43,6 +44,34 @@ in
         '';
         scriptPath = ".xinitrc";
       };
+
+      programs.git = {
+        enable = true;
+        package = pkgs.gitAndTools.gitFull;
+        userName = "Patryk Niedźwiedziński";
+        userEmail = "pniedzwiedzinski19@gmail.com";
+        signing = {
+          key = "pniedzwiedzinski19@gmail.com";
+          signByDefault = true;
+        };
+        aliases = {
+          lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+        };
+
+        extraConfig = {
+          url."ssh://git@github.com/".insteadOf = "https://github.com/";
+          url."ssh://git@github.com/pniedzwiedzinski/".insteadOf = "pn:";
+          url."ssh://git@gitlab.com/".insteadOf = "https://gitlab.com/";
+          url."ssh://git@bitbucket.org/".insteadOf = "https://bitbucket.org/";
+
+          sendemail = {
+            smtpserver = "${pkgs.msmtp}/bin/msmtp";
+            smtpserveroption  = [ "-a" "pniedzwiedzinski19@gmail.com" ];
+          };
+        };
+      };
+
+
     };
 
   ## Although it's less secure I tend not to have sensitive data on my machines
