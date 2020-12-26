607e1bae574753079cff9820fa102885436ed30f
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-12-26 19:20:24 +0100

    Use home-manager@20.09

--- users/pn.nix
+++ users/pn.nix
@@ -3,7 +3,8 @@
 
 let
   home-manager = builtins.fetchTarball {
-    url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+    url = "https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz";
+    sha256 = "0cxnh7g1nagmdkl40jsg5kvfq6mdpg21k7b9y0i713p6zsgd48jl";
   };
 in
   {
