2be6f25d96537e014b4238a91d9878ceec188a78
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-01-22 08:27:01 +0100

    Fix checksum

--- machines/asus/brcm.nix
+++ machines/asus/brcm.nix
@@ -5,13 +5,17 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://android.googlesource.com/platform/hardware/broadcom/wlan/+archive/master/bcmdhd/firmware/bcm43341.tar.gz";
-    sha256 = "1vm5yvxznkvsbdvnhm10dci33i0b9flzqalpbgvmzyd95vi7s4bm";
+    sha256 = "0cd25rrmc0b74pxhhgv1jgjwfk6214pz1s7lffplsc4s354h1wd9";
   };
+
+  sourceRoot = ".";
 
   installPhase = ''
     mkdir -p $out/lib/firmware/brcm
 
     cp fw_bcm43341.bin $out/lib/firmware/brcm/brcmfmac43340-sdio.bin
     cp fw_bcm43341.bin $out/lib/firmware/brcm/brcmfmac43341-sdio.bin
+    cp ${./brcm.txt} $out/lib/firmware/brcm/brcmfmac43340-sdio.txt
+    cp ${./brcm.txt} $out/lib/firmware/brcm/brcmfmac43341-sdio.txt
   '';
 }
