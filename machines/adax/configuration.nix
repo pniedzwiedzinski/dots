c1a819bfe873d266c748429eabaa0603f07c3120
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-01-26 18:40:58 +0100

    Add bob the builder

--- machines/adax/configuration.nix
+++ machines/adax/configuration.nix
@@ -5,12 +5,23 @@
   imports = [
     ../base.nix
     ../pl.nix
-    ../../modules/larbs.nix
+    #../../modules/larbs.nix
   ];
+
 
   networking.hostName = "adax";
 
   environment.systemPackages = with pkgs; [
   ];
+
+  # https://sgt.hootr.club/molten-matter/nix-distributed-builds/
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  services.openssh.enable = true;
+  users.users.bob = {
+    description = "Bob the Builder";
+    isNormalUser = true;
+    createHome = true;
+    shell = "/bin/sh";
+  };
 
 }
