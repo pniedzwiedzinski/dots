843a0baa38ad2f321546e383ba423f9b9818e46a
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2021-10-06 10:09:41 +0200

    Noip

--- machines/srv1/configuration.nix
+++ machines/srv1/configuration.nix
@@ -45,7 +45,7 @@ in
 
     networking.hostName = "srv1";
     networking.extraHosts = ''
-      192.168.1.136 srv1.niedzwiedzinski.cyou git.niedzwiedzinski.cyou tmp.niedzwiedzinski.cyou zhr.niedzwiedzinski.cyou help.niedzwiedzinski.cyou niedzwiedzinski.cyou
+      192.168.1.136 srv1.niedzwiedzinski.cyou git.niedzwiedzinski.cyou tmp.niedzwiedzinski.cyou zhr.niedzwiedzinski.cyou help.niedzwiedzinski.cyou niedzwiedzinski.cyou pics.niedzwiedzinski.cyou
     '' + pkgs.stdenv.lib.readFile ( pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/StevenBlack/hosts/d2be343994aacdec74865ff8d159cf6e46359adf/alternates/fakenews-gambling-porn/hosts";
       sha256 = "1la5rd0znc25q8yd1iwbx22zzqi6941vyzmgar32jx568j856s8j";
@@ -109,6 +109,7 @@ in
     nvim lm_sensors
     mirror
     newrepo
+    noip
   ];
 
   services.openssh.enable = true;
@@ -151,6 +152,11 @@ in
       root = "${www}/srv1.niedzwiedzinski.cyou";
     };
     "pics.srv1.niedzwiedzinski.cyou" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "${www}/pics.srv1.niedzwiedzinski.cyou";
+    };
+    "pics.niedzwiedzinski.cyou" = {
       enableACME = true;
       forceSSL = true;
       root = "${www}/pics.srv1.niedzwiedzinski.cyou";
@@ -229,10 +235,9 @@ in
     keyPath = "/var/lib/acme/niedzwiedzinski.cyou/key.pem";
     docBase = "${www}/niedzwiedzinski.cyou";
   };
-  security.acme.certs."niedzwiedzinski.cyou".allowKeysForGroup = true;
-  systemd.services.molly-brown.serviceConfig.SupplementaryGroups = [ config.security.acme.certs."niedzwiedzinski.cyou".group ];
 
   systemd = {
+    services.molly-brown.serviceConfig.SupplementaryGroups = [ config.security.acme.certs."niedzwiedzinski.cyou".group ];
     services.git-fetch = {
       script = ''
         #!/bin/sh
