8e8a18d024caf6d8736d3d11ef1a65d6e633ca65
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-08-15 19:30:37 +0200

    26 current  2024-08-15 19:30:33  24.05.20240626.89c4987   6.6.35                          *

--- machines/x220-gnome/pass.nix
+++ machines/x220-gnome/pass.nix
@@ -3,6 +3,8 @@
 	environment.systemPackages = with pkgs; [
 		pkgs.pass-wayland
 	];
+	
+	programs.browserpass.enable = true;
 
 	environment.variables = {
 		PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
