bdbd8d094c2af9f88b484011887e1d1d4fe557d5
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-08-15 19:42:43 +0200

    28 current  2024-08-15 19:42:38  24.05.20240626.89c4987   6.6.35                          *

--- machines/x220-gnome/pass.nix
+++ machines/x220-gnome/pass.nix
@@ -6,6 +6,11 @@
 	
 	programs.browserpass.enable = true;
 
+	programs.gnupg.agent = {
+		enable = true;
+		enableSSHSupport = true;
+	};
+
 	environment.variables = {
 		PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
 	};
