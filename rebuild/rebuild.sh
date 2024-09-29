af1a1f5d226b346de4a7ee4b920ffd0239b308f2
Patryk Niedzwiedzinski <patryk@niedzwiedzinski.cyou>
2024-09-29 18:06:35 +0200

    17 current  2024-09-29 18:06:32  24.05.20240928.fbca5e7   6.6.52                          *

--- rebuild/rebuild.sh
+++ rebuild/rebuild.sh
@@ -34,7 +34,7 @@ git diff -U0 '*'
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild switch --flake $DIR/flake.nix &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+(sudo nixos-rebuild switch --flake $DIR/flake.nix 2>&1 | tee nixos-switch.log) || (cat nixos-switch.log | grep --color error && exit 1)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
