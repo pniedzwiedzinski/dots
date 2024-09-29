6485c8739b424f2607fa67a96d9d9971cebda777
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2024-09-29 19:59:31 +0200

    t14: 30 current  2024-09-29 19:59:28  24.05.20240928.fbca5e7   6.6.52                          *

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -39,6 +39,7 @@ in
             				"org/gnome/settings-daemon/plugins/media-keys" = {
               					custom-keybindings = [
                 					"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+                					"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
              					 ];
 					};
 
