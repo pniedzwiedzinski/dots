fe5aa85281d870dcdb52e701315cc02d97946aa3
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2024-10-03 16:14:06 +0200

    t14: 55 current  2024-10-03 16:13:53  24.05.20240928.fbca5e7   6.6.52                          *

--- machines/t14/default.nix
+++ machines/t14/default.nix
@@ -16,19 +16,19 @@
 	time.timeZone = "Europe/Warsaw";
 
 # Select internationalisation properties.
-	i18n.defaultLocale = "en_IE.UTF-8";
+	i18n.defaultLocale = "en_US.UTF-8";
 
 	i18n.extraLocaleSettings = {
-		LC_ALL = "en_IE.UTF-8";
-		LC_ADDRESS = "en_IE.UTF-8";
-		LC_IDENTIFICATION = "en_IE.UTF-8";
-		LC_MEASUREMENT = "en_IE.UTF-8";
-		LC_MONETARY = "en_IE.UTF-8";
+		LC_ALL = "en_US.UTF-8";
+		LC_ADDRESS = "pl_PL.UTF-8";
+		LC_IDENTIFICATION = "pl_PL.UTF-8";
+		LC_MEASUREMENT = "pl_PL.UTF-8";
+		LC_MONETARY = "pl_PL.UTF-8";
 		LC_NAME = "en_IE.UTF-8";
-		LC_NUMERIC = "en_IE.UTF-8";
-		LC_PAPER = "en_IE.UTF-8";
-		LC_TELEPHONE = "en_IE.UTF-8";
-		LC_TIME = "en_IE.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "pl_PL.UTF-8";
+		LC_TELEPHONE = "pl_PL.UTF-8";
+		LC_TIME = "pl_PL.UTF-8";
 	};
 
 	programs.vim.defaultEditor = true;
