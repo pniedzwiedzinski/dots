63df6f75b532931a1353884a883fb8425455bc26
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2024-10-17 14:37:42 +0200

    Uncomment SSL for obsidian

--- modules/obsidian-livesync.nix
+++ modules/obsidian-livesync.nix
@@ -72,8 +72,8 @@ in
 		services.nginx = {
 			enable = true;
 			virtualHosts.${cfg.domain} = {
-#				enableACME = true;
-#				forceSSL = true;
+				enableACME = true;
+				forceSSL = true;
 				locations."/" = {
 					proxyPass = "http://127.0.0.1:${toString couchdb-port}";
 			        	extraConfig = ''
