883a7c8044248ab6c56c5b2c9e391ac3b9fa78d0
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-12-12 22:25:52 +0100

    yggup

--- machines/srv3/services/yggdrasil/default.nix
+++ machines/srv3/services/yggdrasil/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 let
-  port = 1670;
+  port = 22;
 in
 {
 
@@ -15,6 +15,7 @@ in
       AllowedPublicKeys = [
         "af868e700b6caa8debe0f912ea14b7f7c98a09418d2bf13f56e7a9a5ec73b5a8" #srv4
         "e499a4996d79d1e083a61cf024cecdf02cb51a4cd5c27dfbd82bf76b9fa9c904" #t14
+        "2a0b4bb6616f881b094a6336961212c4194b19e438b1822ba151538afdc1e64c" #oppo
       ];
       MulticastInterfaces = [
         {
