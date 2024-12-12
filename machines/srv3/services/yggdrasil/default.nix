e718bb0dee622895f69892ac7ee3d681e5ed9528
Patryk Niedźwiedziński <patryk@niedzwiedzinski.cyou>
2024-12-12 21:49:48 +0100

    Specify nodes

--- machines/srv3/services/yggdrasil/default.nix
+++ machines/srv3/services/yggdrasil/default.nix
@@ -12,7 +12,10 @@ in
         "tls://0.0.0.0:${toString port}" 
       ];
       Peers = [];
-      AllowedPublicKeys = [];
+      AllowedPublicKeys = [
+        "af868e700b6caa8debe0f912ea14b7f7c98a09418d2bf13f56e7a9a5ec73b5a8" #srv4
+        "e499a4996d79d1e083a61cf024cecdf02cb51a4cd5c27dfbd82bf76b9fa9c904" #t14
+      ];
       MulticastInterfaces = [
         {
 	  Regex = ".*";
