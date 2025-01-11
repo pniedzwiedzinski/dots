a08f4d217fff5e352a0b2737b4be7e09210eeaf0
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-01-11 20:26:10 +0100

    t14: 120 current  2025-01-11 20:26:05  24.11.20241205.4dc2fc4  6.6.63                          *

--- flake.nix
+++ flake.nix
@@ -1,84 +1,95 @@
 {
-	description = "Nixos config flake";
+  description = "Nixos config flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-24.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nix-index-database.url = "github:nix-community/nix-index-database";
-    		nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-		ronvim.url = "git+https://codeberg.org/veeronniecaw/ronvim.git?ref=main";
-		disko.url = "github:nix-community/disko";
-  		disko.inputs.nixpkgs.follows = "nixpkgs";
-		impermanence.url = "github:nix-community/impermanence";
-		agenix.url = "github:ryantm/agenix";
-		nix-ld.url = "github:Mic92/nix-ld";
-		nix-ld.inputs.nixpkgs.follows = "nixpkgs";
-	};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    ronvim.url = "git+https://codeberg.org/veeronniecaw/ronvim.git?ref=main";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+    agenix.url = "github:ryantm/agenix";
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
+    pnvf.url = "github:pniedzwiedzinski/pnvf";
+    pnvf.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-	outputs = { self, nixpkgs, ... }@inputs: 
-
-		let
-		nixosSystem = system: name: nixosModules: nixpkgs.lib.nixosSystem {
-		inherit system;
-		specialArgs = {inherit inputs;};
-		modules = nixosModules ++ [
-			({ config, pkgs, ... }:
-		let rebuild = pkgs.writeShellScriptBin "rebuild" (builtins.readFile ./rebuild.sh); in
-			 {
-			 networking.hostName = name;
-			environment.systemPackages = [ rebuild ];
-			 nix = {
-			 extraOptions = "extra-experimental-features = nix-command flakes";
-			 };
-			 })
-		./machines/${name}
-		];
-	};
-	in {
-		nixosConfigurations = {
-			x220-gnome = nixosSystem "x86_64-linux" "x220-gnome" [
-				inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x220
-				inputs.home-manager.nixosModules.default
-				inputs.disko.nixosModules.disko
-				{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.pn = import ./home.nix;
-				}
-			];
-			t14 = nixosSystem "x86_64-linux" "t14" [
-				./modules/gnome.nix
-				./modules/work-gp.nix
-					inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
-					inputs.home-manager.nixosModules.default
-					inputs.nix-index-database.nixosModules.nix-index
-					inputs.agenix.nixosModules.default
-					inputs.nix-ld.nixosModules.nix-ld
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.users.pn = import ./home.nix;
-						environment.systemPackages = [ inputs.ronvim.packages.x86_64-linux.default ];
-					}
-			];
-			x220 = nixosSystem "x86_64-linux" "x220" [
-				inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x220
-				inputs.disko.nixosModules.disko
-				inputs.impermanence.nixosModules.impermanence
-				inputs.home-manager.nixosModules.default
-				{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.pn = import ./home.nix;
-				}
-
-			];
-			srv3 = nixosSystem "x86_64-linux" "srv3" [];
-		};
-	};
+  outputs = {nixpkgs, ...} @ inputs: let
+    nixosSystem = system: name: nixosModules:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit inputs;};
+        modules =
+          nixosModules
+          ++ [
+            ({pkgs, ...}: let
+              rebuild = pkgs.writeShellScriptBin "rebuild" (builtins.readFile ./rebuild.sh);
+            in {
+              networking.hostName = name;
+              environment.systemPackages = [rebuild];
+              nix = {
+                extraOptions = "extra-experimental-features = nix-command flakes";
+              };
+            })
+            ./machines/${name}
+          ];
+      };
+  in {
+    nixosConfigurations = {
+      x220-gnome = nixosSystem "x86_64-linux" "x220-gnome" [
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x220
+        inputs.home-manager.nixosModules.default
+        inputs.disko.nixosModules.disko
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.pn = import ./home.nix;
+          };
+        }
+      ];
+      t14 = nixosSystem "x86_64-linux" "t14" [
+        ./modules/gnome.nix
+        ./modules/work-gp.nix
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
+        inputs.home-manager.nixosModules.default
+        inputs.nix-index-database.nixosModules.nix-index
+        inputs.agenix.nixosModules.default
+        inputs.nix-ld.nixosModules.nix-ld
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.pn = import ./home.nix;
+          };
+          environment.systemPackages = [
+            inputs.ronvim.packages.x86_64-linux.default
+            inputs.pnvf.packages.x86_64-linux.default
+          ];
+        }
+      ];
+      x220 = nixosSystem "x86_64-linux" "x220" [
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x220
+        inputs.disko.nixosModules.disko
+        inputs.impermanence.nixosModules.impermanence
+        inputs.home-manager.nixosModules.default
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.pn = import ./home.nix;
+          };
+        }
+      ];
+      srv3 = nixosSystem "x86_64-linux" "srv3" [];
+    };
+  };
 }
--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,149 +1,151 @@
-{ pkgs, ... }:
-let
-	switch-theme = pkgs.writeShellScriptBin "switch-theme" (builtins.readFile ./switch-theme.sh);
-in
-{
+{pkgs, ...}: let
+  switch-theme = pkgs.writeShellScriptBin "switch-theme" (builtins.readFile ./switch-theme.sh);
+in {
+  imports = [
+    ./gnome-cast.nix
+    ./appset-desktop.nix
+  ];
 
-	imports = [
-		./gnome-cast.nix
-		./appset-desktop.nix
-	];
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-# Enable the X11 windowing system.
-	services.xserver.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.excludePackages = [pkgs.xterm];
 
-# Enable the GNOME Desktop Environment.
-	services.xserver.displayManager.gdm.enable = true;
-	services.xserver.desktopManager.gnome.enable = true;
-	services.xserver.desktopManager.xterm.enable = false;
-	services.xserver.excludePackages = [ pkgs.xterm ];
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    pkgs.epiphany
+    baobab
+    totem
+    yelp
+    file-roller
+    seahorse
+    gnome-clocks
+    pkgs.gnome-connections
+    pkgs.gnome-tour
+  ];
 
-	environment.gnome.excludePackages = with pkgs.gnome; [
-		pkgs.epiphany
-		baobab totem yelp file-roller seahorse gnome-clocks pkgs.gnome-connections
-			pkgs.gnome-tour
-	];
+  programs.dconf = {
+    enable = true;
+    profiles.user.databases = [
+      {
+        lockAll = true;
+        settings = {
+          "org/gnome/shell" = {
+            favorite-apps = ["brave-browser.desktop" "org.gnome.Geary.desktop" "org.gnome.Nautilus.desktop"];
+          };
 
-	programs.dconf = {
-		enable = true;
-		profiles.user.databases = [
-			{
-				lockAll = true;
-				settings = {
-					"org/gnome/shell" = {
-						favorite-apps = [ "brave-browser.desktop" "org.gnome.Geary.desktop" "org.gnome.Nautilus.desktop" ];
-					};
+          "org/gnome/desktop/interface" = {
+            enable-hot-corners = false;
+            show-battery-percentage = true;
+          };
 
-					"org/gnome/desktop/interface" = {
-						enable-hot-corners = false;
-						show-battery-percentage = true;
-					};
+          "org/gnome/desktop/wm/keybindings" = {
+            close = ["<Super>q"];
+          };
 
-					"org/gnome/desktop/wm/keybindings" = {
-						close = ["<Super>q"];
-					};
-					
-            				"org/gnome/settings-daemon/plugins/media-keys" = {
-              					custom-keybindings = [
-                					"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-                					"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-							"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/" 
-             					 ];
-					};
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+            custom-keybindings = [
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+            ];
+          };
 
-            				"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-              					binding = "<Super>Return";
-              					command = "kgx";
-              					name = "GNOME Console";
-            				};
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+            binding = "<Super>Return";
+            command = "kgx";
+            name = "GNOME Console";
+          };
 
-					"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-              					binding = "TaskPane";
-              					command = "switch-theme";
-              					name = "Switch Theme";
-            				};
-					"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-              					binding = "Favorites";
-              					command = "switch-theme";
-              					name = "Switch Theme 2";
-            				};
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+            binding = "TaskPane";
+            command = "switch-theme";
+            name = "Switch Theme";
+          };
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+            binding = "Favorites";
+            command = "switch-theme";
+            name = "Switch Theme 2";
+          };
+        };
+      }
+    ];
+  };
 
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "pl";
+    xkb.variant = "";
+  };
 
-				};
-			}
-		];
-	};
+  # Configure console keymap
+  console.keyMap = "pl2";
 
-# Configure keymap in X11
-	services.xserver = {
-		xkb.layout = "pl";
-		xkb.variant = "";
-	};
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-# Configure console keymap
-	console.keyMap = "pl2";
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-# Enable CUPS to print documents.
-	services.printing.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-# Enable sound with pipewire.
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-# If you want to use JACK applications, uncomment this
-#jack.enable = true;
+  boot.plymouth.enable = true;
 
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+  };
 
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
-	};
+  environment.systemPackages = with pkgs; [
+    switch-theme
+    libnotify
+    gnome-boxes
+    file-roller
+    gnomeExtensions.hibernate-status-button
+    gnomeExtensions.caffeine
+    #brave #specified in home.nix
+    (pkgs.brave.overrideAttrs (oldAttrs: {
+      installPhase =
+        oldAttrs.installPhase
+        + ''
+           	substituteInPlace $out/share/applications/brave-browser.desktop \
+          --replace %U "--profile-directory=Default %U"
+        '';
+    }))
 
-	boot.plymouth.enable = true;
+    newsflash
+    spotify
+    fragments
+    libreoffice
+    signal-desktop
+    vlc
+    wl-clipboard
+    mousai
+    gnome-frog
+    celluloid
+  ];
 
-	programs.kdeconnect = {
-		enable = true;
-		package = pkgs.gnomeExtensions.gsconnect;
-	};
+  fonts.packages = with pkgs; [
+    nerdfonts
+    font-awesome
+    liberation_ttf
+  ];
 
-	environment.systemPackages = with pkgs; [
-		switch-theme
-		libnotify
-		gnome-boxes
-		file-roller
-		gnomeExtensions.hibernate-status-button
-		gnomeExtensions.caffeine
-		#brave #specified in home.nix
-            	(pkgs.brave.overrideAttrs (oldAttrs: {
-		  installPhase = oldAttrs.installPhase + ''
-		  	substituteInPlace $out/share/applications/brave-browser.desktop \
-			--replace %U "--profile-directory=Default %U"
-		  '';
-                }))
+  nixpkgs.config.allowUnfree = true;
 
-		newsflash
-		spotify
-		fragments
-		libreoffice
-		signal-desktop
-		vlc
-		wl-clipboard
-		mousai
-		gnome-frog
-                celluloid
-	];
-
-	fonts.packages = with pkgs; [
-		#nerdfonts
-		font-awesome
-		liberation_ttf
-	];
-
-	nixpkgs.config.allowUnfree = true;
-
-	documentation.nixos.enable = false;
+  documentation.nixos.enable = false;
 }
