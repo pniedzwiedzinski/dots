31f19920bf0e9fefff5e7d86a55a717e0bc7c461
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-02-28 14:18:51 +0100

    chore: remove platforms

--- platforms/linux/aliases.nix
+++
@@ -1,4 +0,0 @@
-{
-  nixos-rebuild = "sudo nixos-rebuild -I nixos-config=$HOME/.config/nixpkgs/platforms/linux/configuration.nix";
-  cf = "v $HOME/.config/nixpkgs/platforms/linux/configuration.nix";
-}
