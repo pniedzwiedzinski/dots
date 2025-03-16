457b4502e5d9a1f057ddb1d3700ce6ff5272404f
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-03-16 11:53:17 +0100

    srv5: auto shutdown negate

--- machines/srv5/auto-shutdown.nix
+++ machines/srv5/auto-shutdown.nix
@@ -4,35 +4,35 @@
     export PATH=${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:${pkgs.docker}/bin:$PATH
 
     check_activity() {
-    	# Check for active SSH sessions
-    	ssh_sessions=$(who | grep -E "(pts|tty)" -c)
+      # Check for active SSH sessions
+      ssh_sessions=$(who | grep -E "(pts|tty)" -c)
 
-    	# Check for running Ollama jobs
-    	ollama_jobs=$(docker exec ollama ollama ps | wc -l)
+      # Check for running Ollama jobs
+      ollama_jobs=$(docker exec ollama ollama ps | wc -l)
 
-    	if [[ "$ssh_sessions" -ge 1 ]]; then
+      if [[ "$ssh_sessions" -ge 1 ]]; then
         echo "SSH active"
         return 1
       fi
       if [[ "$ollama_jobs" -ge 2 ]]; then
         echo "Ollama active"
-    		return 1 # No activity (ollama ps header line counts as 1)
-    	fi
-    	return 0 # Activity detected
+        return 1 # No activity (ollama ps header line counts as 1)
+      fi
+      return 0 # Activity detected
     }
 
     while true; do
-    	if check_activity; then
-    		sleep 60 # Check again in a minute
-    	else
+      if ! check_activity; then
+        sleep 60 # Check again in a minute
+      else
         echo "No SSH and Ollama active, starting timeout"
-    		sleep 300 # No activity, wait 5 more minutes
-    		if ! check_activity; then
-    			echo "No activity detected, shutting down..."
-    			systemctl poweroff
-    		fi
-    	fi
-    	sleep 60
+        sleep 300 # No activity, wait 5 more minutes
+        if check_activity; then
+          echo "No activity detected, shutting down..."
+          systemctl poweroff
+        fi
+      fi
+      sleep 60
     done
   '';
 in {
