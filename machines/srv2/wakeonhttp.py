93055133e3e6b6ce08324e450af619cf619faa41
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-03-26 11:35:58 +0100

    srv2: working wakeonhttp

--- machines/srv2/wakeonhttp.py
+++ machines/srv2/wakeonhttp.py
@@ -8,19 +8,17 @@ app = Flask(__name__)
 # Global configuration variables
 GPIO_PIN: int = 14
 
-
-def power_on_server() -> None:
-    """Activate the server by toggling the GPIO pin."""
-    GPIO.output(GPIO_PIN, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(GPIO_PIN, GPIO.LOW)
-    GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+GPIO.output(GPIO_PIN, GPIO.LOW)
 
 
 @app.route("/power", methods=["GET"])
 def power_on() -> tuple[str, int]:
     """Endpoint to power on the server."""
-    power_on_server()
+    GPIO.output(GPIO_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(GPIO_PIN, GPIO.LOW)
     return "Server Powering On", 200
 
 
@@ -39,16 +37,11 @@ def parse_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def configure_gpio(pin: int) -> None:
-    """Configure the GPIO settings."""
-    print(f"Setting up GPIO PIN: ${pin}")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin, GPIO.OUT)
-
-
 if __name__ == "__main__":
-    args = parse_arguments()
-    GPIO_PIN = args.gpio_pin
+    try:
+        args = parse_arguments()
+        # GPIO_PIN = args.gpio_pin
 
-    configure_gpio(GPIO_PIN)
-    app.run(host="0.0.0.0", port=args.port)
+        app.run(host="0.0.0.0", port=args.port)
+    finally:
+        GPIO.cleanup()
