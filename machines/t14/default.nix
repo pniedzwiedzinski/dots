1a139044809b3cb31244841948054f52c5830c0c
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-05-21 09:19:31 +0200

    t14: 5 current   2025-05-21 09:19:26  24.11.20250515.ba8b70e  6.6.90                          *

--- machines/t14/default.nix
+++ machines/t14/default.nix
@@ -17,6 +17,28 @@
     ./virt.nix
   ];
 
+  # Power management
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 50;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 60; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    };
+  };
+
   disko.devices.disk.main.device = "/dev/nvme0n1";
 
   # Enable networking
