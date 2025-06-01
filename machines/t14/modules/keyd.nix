a36d983e18a9207940aad2d554b8fab18a46fa06
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-06-01 22:52:23 +0200

    t14: 14 current  2025-06-01 22:52:21  24.11.20250515.ba8b70e  6.6.90                          *

--- machines/t14/modules/keyd.nix
+++ machines/t14/modules/keyd.nix
@@ -1,14 +1,14 @@
 {
   services.keyd = {
-    enable = false;
-    keyboards.default = {
-      ids = ["04b3:301e:b108cf48"]; # Applies to all keyboards
-      settings = {
-        main = {
-          leftalt = "leftmeta";
-          "leftcontrol+left" = "left";
-        };
-      };
-    };
+    enable = true;
+    # keyboards.default = {
+    #   ids = ["04b3:301e:b108cf48"]; # Applies to all keyboards
+    #   settings = {
+    #     main = {
+    #       leftalt = "leftmeta";
+    #       "leftcontrol+left" = "left";
+    #     };
+    #   };
+    # };
   };
 }
