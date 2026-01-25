38c874c5e45ad07d41e3a483448f2d403667a50a
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2026-01-25 19:15:11 +0100

    srv2: move to nixos-hardware

--- systems/aarch64-linux/srv2/configuration.nix
+++ systems/aarch64-linux/srv2/configuration.nix
@@ -15,31 +15,32 @@
   srv.enable = true;
   system.autoUpgrade.enable = false;
 
-  # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
-  raspberry-pi-nix.board = "bcm2711";
-  hardware = {
-    raspberry-pi = {
-      config = {
-        all = {
-          base-dt-params = {
-            BOOT_UART = {
-              value = 1;
-              enable = true;
-            };
-            uart_2ndstage = {
-              value = 1;
-              enable = true;
-            };
-          };
-          dt-overlays = {
-            disable-bt = {
-              enable = true;
-              params = {};
-            };
-          };
-        };
-      };
-    };
+  # Partition for config.txt and other overlays
+  fileSystems."/boot/firmware" = {
+    device = "/dev/disk/by-label/FIRMWARE";
+    fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022" "nofail" "noatime"];
   };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+    options = ["noatime"]; # Extends SD card life
+  };
+
+  # Enable U-BOOT
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  # Reboot after 10 seconds if kernel crashes
+  boot.kernelParams = ["panic=10"];
+
+  # Hardware Watchdog: If the kernel freezes entirely (no panic),
+  # the hardware will force a reset after 30 seconds.
+  systemd.settings.Manager.RuntimeWatchdogSec = "20s";
+  systemd.settings.Manager.RebootWatchdogSec = "30s";
+
   security.rtkit.enable = true;
+
+  system.stateVersion = "24.11";
 }
